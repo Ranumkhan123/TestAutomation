23a0d6b9660bc388b51c5ea5d8b0fc1d11e034df
Ranum Khan <ranumkhan123@gmail.com>
2023-01-16 13:16:06 +0500

    TestAutomation

---
+++ SeleniumTestingPractice/TestCases.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeleniumTestingPractice.Pages;
+using System;
+
+namespace SeleniumTestingPractice
+{
+    [TestClass]
+    public class TestCases
+    {
+        AandBTesting abtest = new AandBTesting();
+        AddandRemoveElement adddandremoveelement = new AddandRemoveElement();
+        CheckBoxes checkbox = new CheckBoxes();
+        DisappearingElements elements = new DisappearingElements();
+        DynamicControls dynamicontrols = new DynamicControls();
+        DynamicLoading dynamicloading = new DynamicLoading();
+        EntryAd entryad = new EntryAd();
+        FormAuthentication formauthentication = new FormAuthentication();
+        HorizontalSlider horizontalslider = new HorizontalSlider();
+        Hovers hovers = new Hovers();
+        JavaScriptAlerts alerts = new JavaScriptAlerts();
+        MultipleWindows multiplewindows = new MultipleWindows();
+        NotificationMessages notificationmessage = new NotificationMessages();
+        
+
+
+        [TestMethod]
+        public void ABTesting()
+        {
+
+            CorePage.SeleniumInit();
+            abtest.ABtest("https://the-internet.herokuapp.com/");
+
+            CorePage.driver.Close();
+        }
+
+        [TestMethod]
+        public void AddAndRemoveElements()
+        {
+
+            CorePage.SeleniumInit();
+            adddandremoveelement.AddandRemove("https://the-internet.herokuapp.com/");
+
+            CorePage.driver.Close();
+        }
+
+
+        [TestMethod]
+        public void CheckBoxTestCases()
+        {
+
+            CorePage.SeleniumInit();
+            checkbox.CheckboxesPage("https://the-internet.herokuapp.com/");
+
+            CorePage.driver.Close();
+        }
+
+        [TestMethod]
+        public void DisappearingElementsTestCases()
+        {
+
+            CorePage.SeleniumInit();
+            elements.DisappearingElementsPage("https://the-internet.herokuapp.com/");
+
+            CorePage.driver.Close();
+        }
+
+        [TestMethod]
+        public void DynamicControlsTestCase()
+        {
+
+            CorePage.SeleniumInit();
+            dynamicontrols.DynamicControlsPage("https://the-internet.herokuapp.com/");
+
+            CorePage.driver.Close();
+        }
+
+        [TestMethod]
+        public void DynamicLoadingTestCase()
+        {
+
+            CorePage.SeleniumInit();
+            dynamicloading.DynamicLoadingPage("https://the-internet.herokuapp.com/");
+
+            CorePage.driver.Close();
+        }
+
+        [TestMethod]
+        public void EntryAdTestCase()
+        {
+
+            CorePage.SeleniumInit();
+            entryad.EntryAdPage("https://the-internet.herokuapp.com/");
+
+            CorePage.driver.Close();
+        }
+
+
+        [TestMethod]
+        public void FormAuthenticationTestCase()
+        {
+
+            CorePage.SeleniumInit();
+            formauthentication.FormAuthenticationPage("https://the-internet.herokuapp.com/", "tomsmith", "SuperSecretPassword!");
+
+            CorePage.driver.Close();
+        }
+
+        [TestMethod]
+        public void HorizontalSliderTestCase()
+        {
+
+            CorePage.SeleniumInit();
+            horizontalslider.HorizontalSliderPage("https://the-internet.herokuapp.com/");
+
+            CorePage.driver.Close();
+        }
+
+
+        [TestMethod]
+        public void HoversTestCase()
+        {
+
+            CorePage.SeleniumInit();
+            hovers.HoversPage("https://the-internet.herokuapp.com/");
+
+            CorePage.driver.Close();
+        }
+
+        [TestMethod]
+        public void AlertsTestCase()
+        {
+
+            CorePage.SeleniumInit();
+            alerts.JavaScriptAlertsPage("https://the-internet.herokuapp.com/");
+
+            CorePage.driver.Close();
+        }
+
+        [TestMethod]
+        public void MultipleWindowsTestCase()
+        {
+
+            CorePage.SeleniumInit();
+            multiplewindows.MultipleWindowsPage("https://the-internet.herokuapp.com/");
+
+            CorePage.driver.Close();
+        }
+
+        [TestMethod]
+        public void NotificationMessagesTestCase()
+        {
+
+            CorePage.SeleniumInit();
+            notificationmessage.NotificationMessagesPage("https://the-internet.herokuapp.com/");
+
+            CorePage.driver.Close();
+        }
+
+
+
+    }
+
+}
